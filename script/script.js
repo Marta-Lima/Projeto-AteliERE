5bf22c393d347983d56698e2e1f4de0ba43d088a
MarianaMGSousa <marianamoreira2608@gmail.com>
2022-04-22 17:50:46 -0300

    script JS

---
+++ script/script.js
@@ -0,0 +1,9 @@
+function showMenu() {
+    var element = document.getElementById("list-menu");
+    element.classList.toggle("expand-menu");
+}
+function hideMenu() {
+    var element = document.getElementById("list-menu");
+    element.classList.remove("expand-menu");
+}
+    
